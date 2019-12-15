9e0ff8f16c7e9cfc5e2d2d61a3ce253f1a25e865
Vito Paparella S <vs.paparella@gmail.com>
2019-12-15 13:18:33 +0100

    Formatting improved

--- gitTutorial.py
+++ gitTutorial.py
@@ -1,14 +1,19 @@
-
-print('git touch - create a new file')
-print('git touch .gitignore - this file contains the name of the file ignored by git')
-
 # Initializing repository
 print('git init')
 print("git config --global user.name 'Vito Paparella S' ")
 print("git config --global user.email 'vs.paparella@gmail.com' ")
 
 # Managing files
-print("git add . - add all files and updates in the folder to the source control")
-print("git rm --cached [file name] - remove file")
-print("git status - shows the status of the files")
-print("git commit -m 'comment' - mark changes")
+print(''''git touch
+    creates a new file''')
+print('''git touch .gitignore
+    creates the .gitignore file: this file contains the name of the files ignored by git''')
+print('''git add .
+    adds all not ignored files in the folder to the source control
+    must be used every time a file is updated''')
+print('''git rm --cached [file name]
+    remove a file''')
+print('''git status
+    shows the status of the files''')
+print('''git commit -m 'comment'
+      marks all the added files and changes''')
