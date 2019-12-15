dc995fe0725cf227540fc5541f902e1b1b0dd34d
Vito Paparella S <vs.paparella@gmail.com>
2019-12-15 13:36:43 +0100

    merging tutorial

--- featureA.py
+++ featureA.py
@@ -1,3 +1,9 @@
 # working on new features
 print('This is a new file which is only included in the featureA branch')
 
+# merging branches
+print('''git checkout branchToUpdate
+    select the branch to update''')
+print('''git merge branchToMerge
+    use the command merge to update branchToUpdate with features introduced in branchToMerge
+    it's possible to do this from feature to master and from master to feature''')
