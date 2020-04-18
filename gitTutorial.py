5b1b3694f25a42eeb8dcd8c31d727df482d9f711
Vito Paparella Santorsola <vs.paparella@gmail.com>
2020-04-18 20:16:38 +0200

    clarifications

--- gitTutorial.py
+++ gitTutorial.py
@@ -4,9 +4,9 @@ print("git config --global user.name 'Vito Paparella S' ")
 print("git config --global user.email 'vs.paparella@gmail.com' ")
 
 # Managing files
-print(''''git touch
+print(''''touch
     creates a new file''')
-print('''git touch .gitignore
+print('''touch .gitignore
     creates the .gitignore file: this file contains the name of the files ignored by git''')
 print('''git add .
     adds all not ignored files in the folder to the source control
